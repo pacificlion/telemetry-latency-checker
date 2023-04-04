15079857e0e252bbb0f04c91d1f9f868ed74776e
Prashant Singh <prashants2403@gmail.com>
2023-04-04 15:27:51 -0700

    Added payload in json format

--- receiver.py
+++ receiver.py
@@ -1,7 +1,59 @@
 # Receiver
 import serial
+import json
+import sys
+from datetime import datetime
+import pandas as pd
 
-ser = serial.Serial(port='/dev/ttyUSB0',baudrate=57600,timeout=1)
+date_format_str = '%d/%m/%Y %H:%M:%S.ffffff%f'
+ser = serial.Serial(port='/dev/ttyUSB1',baudrate=57600)
+ser.reset_input_buffer()
+first_ts = -1
+last_ts = -1
+lost_packets = 0
+packets_received=0
+latency = 0
+
+
+def signal_handler():
+    print('You pressed Ctrl+C!')
+    print("============================")
+    print("first time stamp",first_ts)
+    print("last time stamp",last_ts)
+    print("packets lost or out of order",lost_packets)
+    print("# of packets processed",packets_received)
+    if packets_received>0:
+        print("average latency#",latency/packets_received,"ms")
+    sys.exit(0)
+    
+    
 while True:
-    b = ser.read(50)
-    print("voila",b.decode())
+    try:
+        data = ser.readline().decode("utf-8")
+        if data=="":
+            continue
+        try:
+            now = datetime.now()
+            dict_json = json.loads(data)
+            curr_ts = dict_json['ts']
+            start_time = dict_json['time']
+            if first_ts == -1:
+                first_ts = curr_ts
+                last_ts = curr_ts
+                
+            if last_ts >= curr_ts:
+                print("packet out of order, expected  :", (last_ts+1),"received this instead:",curr_ts)
+                lost_packets+=1
+                continue
+            start = pd.to_datetime(start_time, format=date_format_str)
+            
+            
+            # current_time = now.strftime(date_format_str)
+            # print("voila diff",(now-start).microseconds)
+            latency+=((now-start).microseconds/1000)
+            last_ts = curr_ts
+            packets_received+=1
+        except json.JSONDecodeError as e:
+            print("JSON error:",e)
+    except KeyboardInterrupt:
+        signal_handler()
--- transmitter.py
+++ transmitter.py
@@ -1,8 +1,35 @@
 import serial
 import time
+import json
+from datetime import datetime
+import random
+import string
 
+def randStr(chars = string.ascii_uppercase + string.digits, N=10):
+	return ''.join(random.choice(chars) for _ in range(N))
+
+date_format_str = '%d/%m/%Y %H:%M:%S.ffffff%f'
 ser = serial.Serial(port='/dev/ttyUSB0',baudrate=57600)
+ser.reset_input_buffer()
+timestamp = 0
 while True:
-    ser.write(b'Hi Prashant!')
-    time.sleep(2.0)
-    print("sending data")
+    
+    time.sleep(0.02)
+    data ={}
+    
+
+    data["ts"] = timestamp
+    data["payload"] = randStr(chars=string.ascii_lowercase, N=1000)
+    now = datetime.now()
+    current_time = now.strftime(date_format_str)
+    data["time"] = current_time
+    
+    timestamp +=1
+    
+    data=json.dumps(data)
+    data = data+"\n"
+    
+    ser.write(data.encode('ascii'))
+    ser.flush() 
+    
+    print("sending data",timestamp,", of bytes:",data.__sizeof__())
